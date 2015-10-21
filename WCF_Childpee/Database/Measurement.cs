5d0388526047b2d7ac29a01e189ff2fba05ba07a
Martin <Mart7972@edu.eal.dk>
2015-10-21 10:31:42 +0200

    WCF_Childpee
    Version 1

---
+++ WCF_Childpee/Database/Measurement.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Database
+{
+    class Measurement
+    {
+        public DateTime Time { get; set; }
+        public double Value { get; set; }
+        public int Id { get; set; }
+    }
+}
